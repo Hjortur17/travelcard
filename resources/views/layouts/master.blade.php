83bc1ef1139fb8f0812e58db6241186e042810b8
Hjörtur Freyr <hjorturfreyr@hjorturfreyr.com>
2019-01-27 22:55:44 +0100

    Added Google Analytics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,6 +7,21 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
 	<title>Travelcard Iceland</title>
+	<meta name="description" content="Great way to travel cheap and easy around Iceland.">
+	<meta name="keywords" content="Travel,Cheap,Iceland,Camping,Cheap travel in iceland">
+  	<meta name="author" content="Travelcard Iceland">
+
+  	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-133292402-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-133292402-1');
+	</script>
+
+
 
 	<link rel="stylesheet" href="/css/app.css">
 	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-LRlmVvLKVApDVGuspQFnRQJjkv0P7/YFrw84YYQtmYG4nK8c+M+NlmYDCv0rKWpG" crossorigin="anonymous">
