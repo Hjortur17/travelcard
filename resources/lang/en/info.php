b7828b412f4da999629fe59ef7f178795011ffab
Hjörtur Freyr <hjorturfreyr@hjorturfreyr.com>
2019-01-31 23:37:54 +0100

    Translation -fix

--- resources/lang/en/info.php
+++ resources/lang/en/info.php
@@ -1,7 +1,7 @@
 <?php  
 	// ICELANDIC
 	return [
-		'welcome'		=>	'Useful Information'
+		'welcome'		=>	'Useful Information',
 		'phone' 		=> 	'Phone',
 		'weather' 		=> 	'Weather',
 		'see_more' 		=> 	'See the weather in Iceland',
